04700e5f7c22873e2b4afddc8ecca84e9e85b05d
Kerimov056 <ulvisk@code.edu.az>
2023-07-28 04:04:14 -0700

    AddInfrastructureServices

---
+++ src/Infrastructure/EndProject.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EndProject.Infrastructure;
+
+public static class ServiceRegistration
+{
+    public static void AddInfrastructureServices(this IServiceCollection services)
+    {
+
+    }
+}
