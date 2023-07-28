2312c74795a0e6791278fcae4a92365308db8e81
Kerimov056 <ulvisk@code.edu.az>
2023-07-28 11:17:29 -0700

    AuthController

--- src/Presentation/EndProject.API/Controllers/AuthController.cs
+++ src/Presentation/EndProject.API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EndProject.Application.Abstraction.Services;
+using EndProject.Application.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EndProject.API.Controllersş
@@ -7,6 +9,29 @@ namespace EndProject.API.Controllersş
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService) => _authService = authService;
 
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO registerDTO)
+        {
+            await _authService.Register(registerDTO);
+            return Ok();
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+        {
+            var responseToken = await _authService.Login(loginDTO);
+            return Ok();
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> RefreshToken([FromQuery] string ReRefreshtoken)
+        {
+            var response = await _authService.ValidRefleshToken(ReRefreshtoken);
+            return Ok(response);
+        }
     }
 }
