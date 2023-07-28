a4302921ee5490692506ac9d18dc88b9a9edb199
Kerimov056 <ulvisk@code.edu.az>
2023-07-28 05:43:16 -0700

    WriteRepository

--- src/Infrastructure/EndProjet.Persistance/Implementations/WriteRepository.cs
+++ src/Infrastructure/EndProjet.Persistance/Implementations/WriteRepository.cs
@@ -1,40 +1,25 @@
 ﻿using EndProject.Application.Abstraction.Repositories;
 using EndProject.Domain.Entitys.Common;
+using EndProjet.Persistance.Context;
 using Microsoft.EntityFrameworkCore;
 
 namespace EndProjet.Persistance.Implementations;
 
 public class WriteRepository<T> : IWriteRepository<T> where T : BaseEntity, new()
 {
+    private readonly AppDbContext _context;
+    public WriteRepository(AppDbContext context) => _context = context;
     public DbSet<T> Table => throw new NotImplementedException();
 
-    public Task AddAsync(T entity)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task AddAsync(T entity) => await Table.AddAsync(entity);
 
-    public Task AddRangeAsync(IEnumerable<T> entities)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task AddRangeAsync(IEnumerable<T> entities) => await Table.AddRangeAsync(entities);
 
-    public void Remove(T entity)
-    {
-        throw new NotImplementedException();
-    }
+    public void Remove(T entity) => Table.Remove(entity);
 
-    public void RemoveRange(IEnumerable<T> entities)
-    {
-        throw new NotImplementedException();
-    }
+    public void RemoveRange(IEnumerable<T> entities) => Table.RemoveRange(entities);
+    public void Update(T entity) => Table.Update(entity);
 
-    public Task SavaChangeAsync()
-    {
-        throw new NotImplementedException();
-    }
+    public async Task SavaChangeAsync() => await _context.SaveChangesAsync();
 
-    public void Update(T entity)
-    {
-        throw new NotImplementedException();
-    }
 }
