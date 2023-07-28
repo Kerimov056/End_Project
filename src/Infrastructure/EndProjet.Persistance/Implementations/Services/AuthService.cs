2ea5c11cbaef118f055cf5a27a7a1cff74c7c4db
Kerimov056 <ulvisk@code.edu.az>
2023-07-28 10:11:11 -0700

    IAuthService

---
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Services/AuthService.cs
@@ -0,0 +1,22 @@
+﻿using EndProject.Application.Abstraction.Services;
+using EndProject.Application.DTOs;
+
+namespace EndProjet.Persistance.Implementations.Services;
+
+public class AuthService : IAuthService
+{
+    public Task<TokenResponseDTO> Login(LoginDTO loginDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Register(RegisterDTO registerDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TokenResponseDTO> ValidRefleshToken(string refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+}
