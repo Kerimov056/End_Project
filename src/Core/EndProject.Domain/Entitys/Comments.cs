5908c6d78fa24628e0ad2d5be620fc282c558b4d
Kerimov056 <ulvisk@code.edu.az>
2023-08-04 09:51:12 -0700

    Comments

---
+++ src/Core/EndProject.Domain/Entitys/Comments.cs
@@ -0,0 +1,10 @@
+﻿using EndProject.Domain.Entitys.Common;
+
+namespace EndProject.Domain.Entitys;
+
+public class Comments:BaseEntity
+{
+    public string message { get; set; }
+    public Guid PostsId { get; set; }
+    public Posts Posts { get; set; }
+}
--- src/Core/EndProject.Domain/Entitys/Posts.cs
+++ src/Core/EndProject.Domain/Entitys/Posts.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EndProject.Domain.Entitys.Common;
+using EndProject.Domain.Entitys.Identity;
 
-namespace EndProject.Domain.Entitys
+namespace EndProject.Domain.Entitys;
+
+public class Posts:BaseEntity
 {
-    internal class Posts
-    {
-    }
+    public string AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
+    public string? message { get; set; }
+    public List<PostImage>? postImage { get; set; }
+    public List<Comments> comments { get; set; }
 }
