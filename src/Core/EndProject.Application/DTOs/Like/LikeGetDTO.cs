a56d8a42a740891e7569013560691acb233c20a1
Kerimov056 <ulvisk@code.edu.az>
2023-08-04 14:35:01 -0700

    DTO's

---
+++ src/Core/EndProject.Application/DTOs/Like/LikeGetDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace EndProject.Application.DTOs.Like;
+
+public class LikeGetDTO
+{
+    public Guid Id { get; set; }
+    public int likeSum { get; set; }
+    public Guid CommentId { get; set; }
+    public Guid AppUserId { get; set; }
+
+}
