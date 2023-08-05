a4ef77ede7aac9c5cc6fdb2d880d2e08e1b529ea
Kerimov056 <ulvisk@code.edu.az>
2023-08-05 05:46:15 -0700

    IPostService

--- src/Core/EndProject.Application/Abstraction/Services/IPostService.cs
+++ src/Core/EndProject.Application/Abstraction/Services/IPostService.cs
@@ -4,17 +4,9 @@ namespace EndProject.Application.Abstraction.Services;
 
 public interface IPostService
 {
-    //Task<List<PostGetDTO>> GettAllAsync();
-    //Task AddAsync(PostCreateDTO postCreateDTO);
-    //Task<PostGetDTO> GetByIdAsync(Guid Id);
-    //Task UpdateAsync(Guid Id, PostUpdateDTO postUpdateDTO);
-    //Task RemoveAsync(Guid Id);
+    Task<List<PostGetDTO>> GettAllAsync();
+    Task AddAsync(PostCreateDTO postCreateDTO);
+    Task<PostGetDTO> GetByIdAsync(Guid Id);
+    Task UpdateAsync(Guid Id, PostUpdateDTO postUpdateDTO);
+    Task RemoveAsync(Guid Id);
 }
-
-
-//TagGetDTO
-//TagCreateDTO
-//TagUpdateDTO
-//Post_TagGetDTO
-//Post_TagCreateDTO
-//Post_TagUpdateDTO
---
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Services/PostService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Application.Abstraction.Services;
+using EndProject.Application.DTOs.Post;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.Implementations.Services;
+
+public class PostService : IPostService
+{
+    private readonly IPostReadRepository _postReadRepository;
+    private readonly IPostWriteRepository _postWriteRepository;
+    private readonly IMapper _mapper;
+
+    public PostService(IPostReadRepository postReadRepository,
+                       IPostWriteRepository postWriteRepository,
+                       IMapper mapper)
+    {
+        _postReadRepository = postReadRepository;
+        _postWriteRepository = postWriteRepository;
+        _mapper = mapper;
+    }
+
+    public Task AddAsync(PostCreateDTO postCreateDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PostGetDTO> GetByIdAsync(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<PostGetDTO>> GettAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Guid Id, PostUpdateDTO postUpdateDTO)
+    {
+        throw new NotImplementedException();
+    }
+}
