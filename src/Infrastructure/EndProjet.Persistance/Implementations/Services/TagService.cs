749f218fa1a911581cf7b5d03f1f500e5d74fcfb
Kerimov056 <ulvisk@code.edu.az>
2023-08-05 07:55:48 -0700

    CreateTag, GetByIdtag

--- src/Infrastructure/EndProjet.Persistance/Implementations/Services/TagService.cs
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Services/TagService.cs
@@ -1,18 +1,40 @@
-﻿using EndProject.Application.Abstraction.Services;
+﻿using AutoMapper;
+using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Application.Abstraction.Services;
 using EndProject.Application.DTOs.Tag;
+using EndProject.Domain.Entitys;
+using EndProjet.Persistance.Exceptions;
 
 namespace EndProjet.Persistance.Implementations.Services;
 
 public class TagService : ITagService
 {
-    public Task AddAsync(TagCreateDTO tagsCreateDTO)
+    private readonly ITagReadRepository _tagReadRepository;
+    private readonly ITagWriteRepository _tagWriteRepository;
+    private readonly IMapper _mapper;
+
+    public TagService(ITagReadRepository tagReadRepository,
+                      ITagWriteRepository tagWriteRepository,
+                      IMapper mapper)
     {
-        throw new NotImplementedException();
+        _tagReadRepository = tagReadRepository;
+        _tagWriteRepository = tagWriteRepository;
+        _mapper = mapper;
     }
 
-    public Task<TagGetDTO> GetByIdAsync(Guid Id)
+    public async Task AddAsync(TagCreateDTO tagsCreateDTO)
     {
-        throw new NotImplementedException();
+        var NewTag = _mapper.Map<Tags>(tagsCreateDTO);
+        await _tagWriteRepository.AddAsync(NewTag);
+        await _tagWriteRepository.SavaChangeAsync();
+    }
+
+    public async Task<TagGetDTO> GetByIdAsync(Guid Id)
+    {
+        var ByTag = await _tagReadRepository.GetByIdAsync(Id);
+        if (ByTag is null) throw new NotFoundException("Tag is Null");
+        var MapByTag = _mapper.Map<TagGetDTO>(ByTag);
+        return MapByTag;
     }
 
     public Task<List<TagGetDTO>> GettAllAsync()
