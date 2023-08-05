baaa0a8b6f4e19dd76ef4d3108610b676944c323
Kerimov056 <ulvisk@code.edu.az>
2023-08-05 03:13:50 -0700

    PostCreateDTO, PostImageCreateDTO, CommentCreateDTO, PostLikeCreateDTO    <-AllValidator

---
+++ src/Core/EndProject.Application/Validators/PostValidator/PostImageCreateDTOValidator.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Application.DTOs.Post;
+using FluentValidation;
+
+namespace EndProject.Application.Validators.PostValidator;
+
+public class PostImageCreateDTOValidator:AbstractValidator<PostImageCreateDTO>
+{
+	public PostImageCreateDTOValidator()
+	{
+        RuleFor(x => x.ImagePath).NotNull().NotEmpty().MaximumLength(756);
+    }
+}
