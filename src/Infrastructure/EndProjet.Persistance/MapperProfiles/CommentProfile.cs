5541126dee19c2e83dc91668f77f007a21aa6ec8
Kerimov056 <ulvisk@code.edu.az>
2023-08-05 01:26:23 -0700

    Comment && Like Profile Mapper

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/CommentProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Comments;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class CommentProfile:Profile
+{
+	public CommentProfile()
+	{
+		CreateMap<Comments,CommentGetDTO>().ReverseMap();
+	}
+}
---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/LikeProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Like;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class LikeProfile:Profile
+{
+	public LikeProfile()
+	{
+		CreateMap<Like, LikeGetDTO>().ReverseMap();
+	}
+}
