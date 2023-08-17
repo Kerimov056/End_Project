c395d105b59ad05b1a43489bec4ff16c951e0de1
Kerimov056 <ulvisk@code.edu.az>
2023-08-17 13:11:39 -0700

    CarCategory DTO && Profile

---
+++ src/Core/EndProject.Application/DTOs/Category/CarCategoryUpdateDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace EndProject.Application.DTOs.Category;
+
+public class CarCategoryUpdateDTO
+{
+    public string category { get; set; }
+    public Guid CarId { get; set; }
+}
---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/CarCategoryProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Category;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class CarCategoryProfile:Profile
+{
+    public CarCategoryProfile()
+    {
+        CreateMap<CarCategory, CarCategoryGetDTO>().ReverseMap();
+        CreateMap<CarCategory, CarCategoryCreateDTO>().ReverseMap();
+        CreateMap<CarCategory, CarCategoryUpdateDTO>().ReverseMap();
+    }
+}
