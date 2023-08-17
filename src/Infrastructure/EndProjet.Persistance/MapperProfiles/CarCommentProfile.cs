0935b095e1a4d8d48f1484f0cc604e876644bd73
Kerimov056 <ulvisk@code.edu.az>
2023-08-17 16:29:45 -0700

    CarCommentProfile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/CarCommentProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.CarComment;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class CarCommentProfile:Profile
+{
+    public CarCommentProfile()
+    {
+        CreateMap<CarComment, CarCommentGetDTO>().ReverseMap();
+        CreateMap<CarComment, CarCommentCreateDTO>().ReverseMap();
+        CreateMap<CarComment, CarCommentUpdateDTO>().ReverseMap();
+    }
+}
