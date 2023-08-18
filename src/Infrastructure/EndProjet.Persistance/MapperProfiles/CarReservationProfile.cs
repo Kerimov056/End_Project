626c5b5b9c0c103502f553fe6bcdf71ab211fae9
Kerimov056 <ulvisk@code.edu.az>
2023-08-18 06:37:37 -0700

    CarReservation Profile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/CarReservationProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.CarReservation;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class CarReservationProfile:Profile
+{
+    public CarReservationProfile()
+    {
+        CreateMap<CarReservation, CarReservationCreateDTO>().ReverseMap();
+        CreateMap<CarReservation, CarReservationGetDTO>().ReverseMap();
+        CreateMap<CarReservation, CarReservationUpdateDTO>().ReverseMap();
+    }
+}
