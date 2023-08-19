10bdcc054e7ad78353deee0b4644e87d3ae5c2c8
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 08:37:36 -0700

    Advantage class Repository

---
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Repositories/EntityRepository/AdvantageReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Domain.Entitys.Identity;
+using EndProjet.Persistance.Context;
+
+namespace EndProjet.Persistance.Implementations.Repositories.EntityRepository;
+
+public class AdvantageReadRepository : ReadRepository<Advantage>, IAdvantageReadRepository
+{
+    public AdvantageReadRepository(AppDbContext context) : base(context)
+    {
+    }
+}
---
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Repositories/EntityRepository/AdvantageWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Domain.Entitys.Identity;
+using EndProjet.Persistance.Context;
+
+namespace EndProjet.Persistance.Implementations.Repositories.EntityRepository;
+
+public class AdvantageWriteRepository : WriteRepository<Advantage>, IAdvantageWriteRepository
+{
+    public AdvantageWriteRepository(AppDbContext context) : base(context)
+    {
+    }
+}
