309717524e415cbb9ba1efb3d27e7541e16b2fb3
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 08:48:41 -0700

    Advantage Service Dependices Injaction

--- src/Infrastructure/EndProjet.Persistance/Implementations/Services/AdvantageServices.cs
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Services/AdvantageServices.cs
@@ -1,10 +1,25 @@
-﻿using EndProject.Application.Abstraction.Services;
+﻿using AutoMapper;
+using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Application.Abstraction.Services;
 using EndProject.Application.DTOs.Advantage;
 
 namespace EndProjet.Persistance.Implementations.Services;
 
 public class AdvantageServices : IAdvantageServices
 {
+    private readonly IAdvantageReadRepository _readRepository;
+    private readonly IAdvantageWriteRepository _writeRepository;    
+    private readonly IMapper _mapper;
+
+    public AdvantageServices(IAdvantageReadRepository readRepository,
+                             IAdvantageWriteRepository writeRepository,
+                             IMapper mapper)
+    {
+        _readRepository = readRepository;
+        _writeRepository = writeRepository;
+        _mapper = mapper;
+    }
+
     public Task CreateAsync(AdvantageCreateDTO advantageCreateDTO)
     {
         throw new NotImplementedException();
