56eee7ebe6a1b14882a3a90b083e29a6391f65ab
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 08:45:37 -0700

    Advantage Profile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/AdvantageProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Advantage;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class AdvantageProfile:Profile
+{
+    public AdvantageProfile()
+    {
+        CreateMap<Advantage, AdvantageGetDTO>().ReverseMap();
+        CreateMap<Advantage, AdvantageCreateDTO>().ReverseMap();
+        CreateMap<Advantage, AdvantageUpdateDTO>().ReverseMap();
+    }
+}
