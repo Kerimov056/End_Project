ce6b5cab3ff0d15e73e4b870dc4851405a9729ca
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 09:49:06 -0700

    FaqsControllers

---
+++ src/Presentation/EndProject.API/Controllers/FaqsController.cs
@@ -0,0 +1,53 @@
+﻿using EndProject.Application.Abstraction.Services;
+using EndProject.Application.DTOs.Advantage;
+using EndProject.Application.DTOs.Faq;
+using EndProjet.Persistance.Implementations.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace EndProject.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FaqsController : ControllerBase
+{
+    private readonly IFaqServices _faqServices;
+
+    public FaqsController(IFaqServices faqServices)
+    => _faqServices = faqServices;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var Slider = await _faqServices.GetAllAsync();
+        return Ok(Slider);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromForm] FaqCreateDTO faqCreateDTO)
+    {
+        await _faqServices.CreateAsync(faqCreateDTO);
+        return StatusCode((int)HttpStatusCode.Created);
+    }
+
+    [HttpGet("{id:Guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var BySlider = await _faqServices.GetByIdAsync(id);
+        return Ok(BySlider);
+    }
+
+    [HttpDelete("{id:Guid}")]
+    public async Task<IActionResult> Remove(Guid id)
+    {
+        await _faqServices.RemoveAsync(id);
+        return Ok();
+    }
+
+    [HttpPut("{id:Guid}")]
+    public async Task<IActionResult> Uptade(Guid id, [FromForm] FaqUpdateDTO faqUpdateDTO)
+    {
+        await _faqServices.UpdateAsync(id, faqUpdateDTO);
+        return Ok();
+    }
+}
