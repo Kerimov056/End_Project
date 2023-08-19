fcb9a0c30db5255ca81292d1b93ad0d27cde32d5
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 10:30:47 -0700

    BlogImageProfile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/BlogImageProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.BlogImage;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class BlogImageProfile : Profile
+{
+    public BlogImageProfile()
+    {
+        CreateMap<BlogImage, BlogImageGetDTO>().ReverseMap();
+        CreateMap<BlogImage, BlogImageCreateDTO>().ReverseMap();
+        CreateMap<BlogImage, BlogImageUpdateDTO>().ReverseMap();
+    }
+}
