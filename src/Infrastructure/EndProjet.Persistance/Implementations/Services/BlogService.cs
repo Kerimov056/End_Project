91c830e049a4be2ad0357e5be98290646b38a3bf
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 11:50:58 -0700

    BlogServices Dependis Injaction

--- src/Infrastructure/EndProjet.Persistance/Implementations/Services/BlogService.cs
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Services/BlogService.cs
@@ -1,10 +1,28 @@
-﻿using EndProject.Application.Abstraction.Services;
+﻿using AutoMapper;
+using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Application.Abstraction.Services;
 using EndProject.Application.DTOs.Blog;
 
 namespace EndProjet.Persistance.Implementations.Services;
 
 public class BlogService : IBlogService
 {
+    private readonly IBlogReadRepository _blogReadRepository;
+    private readonly IBlogWriteRepository _blogWriteRepository;
+    private readonly IMapper _mapper;
+    private readonly IBlogImageServices _blogImageServices;
+
+    public BlogService(IBlogReadRepository blogReadRepository,
+                       IBlogWriteRepository blogWriteRepository,
+                       IMapper mapper,
+                       IBlogImageServices blogImageServices)
+    {
+        _blogReadRepository = blogReadRepository;
+        _blogWriteRepository = blogWriteRepository;
+        _mapper = mapper;
+        _blogImageServices = blogImageServices;
+    }
+
     public Task CreateAsync(BlogCreateDTO blogCreateDTO)
     {
         throw new NotImplementedException();
