9c378a7aaf5972d4de6ae18d67bbb9a0e7b3e722
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 10:37:12 -0700

    Blog class Reposiyory

---
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Repositories/EntityRepository/BlogReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Domain.Entitys;
+using EndProjet.Persistance.Context;
+
+namespace EndProjet.Persistance.Implementations.Repositories.EntityRepository;
+
+public class BlogReadRepository : ReadRepository<Blog>, IBlogReadRepository
+{
+    public BlogReadRepository(AppDbContext context) : base(context)
+    {
+    }
+}
---
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Repositories/EntityRepository/BlogWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Domain.Entitys;
+using EndProjet.Persistance.Context;
+
+namespace EndProjet.Persistance.Implementations.Repositories.EntityRepository;
+
+public class BlogWriteRepository : WriteRepository<Blog>, IBlogWriteRepository
+{
+    public BlogWriteRepository(AppDbContext context) : base(context)
+    {
+    }
+}
