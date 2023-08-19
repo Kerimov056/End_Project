a75a3b553b6d37cb6e743aa6c822d39db63120b0
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 09:09:46 -0700

    Faq Entity

---
+++ src/Core/EndProject.Domain/Entitys/Faq.cs
@@ -0,0 +1,9 @@
+﻿using EndProject.Domain.Entitys.Common;
+
+namespace EndProject.Domain.Entitys;
+
+public class Faq:BaseEntity
+{
+    public string Title { get; set; }
+    public string Descrption { get; set; }
+}
