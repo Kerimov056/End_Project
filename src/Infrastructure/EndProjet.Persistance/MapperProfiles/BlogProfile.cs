d736e5bdd6e9545d6ba9264b372cdbe2f119f43d
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 10:29:03 -0700

    BlogProfile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/BlogProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Blog;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class BlogProfile : Profile
+{
+    public BlogProfile()
+    {
+        CreateMap<Blog, BlogGetDTO>().ReverseMap();
+        CreateMap<Blog, BlogCreateDTO>().ReverseMap();
+        CreateMap<Blog, BlogUpdateDTO>().ReverseMap();
+    }
+}
