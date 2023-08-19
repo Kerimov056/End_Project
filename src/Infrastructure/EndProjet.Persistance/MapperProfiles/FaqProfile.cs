bb40bf602ee60d9c5093196c0467f94073769999
Kerimov056 <ulvisk@code.edu.az>
2023-08-19 09:15:06 -0700

    FaqProfile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/FaqProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Faq;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class FaqProfile:Profile
+{
+    public FaqProfile()
+    {
+        CreateMap<Faq, FaqGetDTO>().ReverseMap();
+        CreateMap<Faq, FaqCreateDTO>().ReverseMap();
+        CreateMap<Faq, FaqUpdateDTO>().ReverseMap();
+    }
+}
