55e133e0d1baceb33846d08077c58f3c005f9f21
Kerimov056 <ulvisk@code.edu.az>
2023-08-20 01:55:08 -0700

    CarReservationBackGroundServices

---
+++ src/Presentation/EndProject.API/BackGroundServıces/ReservationCheckService.cs
@@ -0,0 +1,36 @@
+﻿using EndProject.Application.Abstraction.Services;
+
+namespace EndProject.API.BackGroundServıces;
+
+public class ReservationCheckService : BackgroundService
+{
+    private readonly ICarReservationServices _carReservationServices;
+    //private
+    public ReservationCheckService(ICarReservationServices carReservationServices)
+        => _carReservationServices = carReservationServices;
+    private Timer _timer;
+        
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            //await
+
+
+            await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
+        }
+    }
+
+    private async Task CheckReservationsAsync()
+    {
+        var ByCars = await _carReservationServices.GetAllAsync();
+        var today = DateTime.Today;
+        foreach (var car in ByCars)
+        {
+            if (car.ReturnDate.Day == today.Day && car.ReturnDate.Month == today.Month && car.ReturnDate.Year == today.Year)
+            {
+                
+            }   
+        }
+    }
+}
