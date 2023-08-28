77cec09a73b6a918565b6d15ec1bbd1f6991fabe
Kerimov056 <ulvisk@code.edu.az>
2023-08-28 04:53:51 -0700

    Faq create,get,update validators

---
+++ src/Core/EndProject.Application/Validators/FaqValidators/FaqCreateDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Application.DTOs.Faq;
+using FluentValidation;
+
+namespace EndProject.Application.Validators.FaqValidators;
+
+public class FaqCreateDtoValidator : AbstractValidator<FaqCreateDTO>
+{
+    public FaqCreateDtoValidator()
+    {
+        RuleFor(x => x.Title).NotNull().NotEmpty().MaximumLength(100);
+        RuleFor(x => x.Descrption).NotNull().NotEmpty().MaximumLength(1000);
+    }
+}
---
+++ src/Core/EndProject.Application/Validators/FaqValidators/FaqGetDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Application.DTOs.Faq;
+using FluentValidation;
+
+namespace EndProject.Application.Validators.FaqValidators;
+
+public class FaqGetDtoValidator : AbstractValidator<FaqGetDTO>
+{
+    public FaqGetDtoValidator()
+    {
+        RuleFor(x => x.Title).NotNull().NotEmpty().MaximumLength(100);
+        RuleFor(x => x.Descrption).NotNull().NotEmpty().MaximumLength(1000);
+    }
+}
