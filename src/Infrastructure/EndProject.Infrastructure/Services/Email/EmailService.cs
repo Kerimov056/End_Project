9e79f40e5c7f1ba0842a080870e16e03f9217caa
Kerimov056 <ulvisk@code.edu.az>
2023-08-27 21:41:29 -0700

    IEMailSettings

---
+++ src/Infrastructure/EndProject.Infrastructure/Services/Email/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using EndProject.Application.Abstraction.Services;
+using Microsoft.Extensions.Options;
+
+namespace EndProject.Infrastructure.Services.Email;
+
+public class EmailService : IEmailService
+{
+    //private readonly EmailSetting _emailSetting;
+    //public EmailService(IOptions<EmailSetting> emailSetting)
+    //{
+    //    _emailSetting = emailSetting.Value;
+    //}
+    public void Send(string to, string subject, string html, string from = null)
+    {
+        throw new NotImplementedException();
+    }
+}
