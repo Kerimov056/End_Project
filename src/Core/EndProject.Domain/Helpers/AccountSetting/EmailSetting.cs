33b18cb200f81de6750a5895b9c9faf8953d797b
Kerimov056 <ulvisk@code.edu.az>
2023-08-27 21:50:25 -0700

    EmailService

--- src/Core/EndProject.Domain/Helpers/AccountSetting/EmailSetting.cs
+++ src/Core/EndProject.Domain/Helpers/AccountSetting/EmailSetting.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace EndProject.Domain.Helpers.AccountSetting;
 
-namespace EndProject.Domain.Helpers.AccountSetting
+public class EmailSetting
 {
-    internal class EmailSetting
-    {
-    }
+    public string From { get; set; }
+    public string SmtpServer { get; set; }
+    public int Port { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
 }
--- src/Infrastructure/EndProject.Infrastructure/Services/Email/EmailService.cs
+++ src/Infrastructure/EndProject.Infrastructure/Services/Email/EmailService.cs
@@ -1,17 +1,32 @@
 ﻿using EndProject.Application.Abstraction.Services;
+using EndProject.Domain.Helpers.AccountSetting;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text;
 
 namespace EndProject.Infrastructure.Services.Email;
 
 public class EmailService : IEmailService
 {
-    //private readonly EmailSetting _emailSetting;
-    //public EmailService(IOptions<EmailSetting> emailSetting)
-    //{
-    //    _emailSetting = emailSetting.Value;
-    //}
+    private readonly EmailSetting _emailSetting;
+    public EmailService(IOptions<EmailSetting> emailSetting)
+     =>  _emailSetting = emailSetting.Value;
     public void Send(string to, string subject, string html, string from = null)
     {
-        throw new NotImplementedException();
+        // create email message
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse(from ?? _emailSetting.From));
+        email.To.Add(MailboxAddress.Parse(to));
+        email.Subject = subject;
+        email.Body = new TextPart(TextFormat.Html) { Text = html };
+
+        // send email
+        using var smtp = new SmtpClient();
+        smtp.Connect(_emailSetting.SmtpServer, _emailSetting.Port, SecureSocketOptions.StartTls);
+        smtp.Authenticate(_emailSetting.Username, _emailSetting.Password);
+        smtp.Send(email);
+        smtp.Disconnect(true);
     }
 }
