8322686df29b69b4b1844e78611ecfb127c728ae
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 03:02:22 +0400

    Blog Validators

---
+++ src/Core/EndProject.Application/Validators/BlogValidators/BlogGetDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Application.DTOs.Blog;
+using FluentValidation;
+
+namespace EndProject.Application.Validators.BlogValidators;
+
+public class BlogGetDtoValidator : AbstractValidator<BlogGetDTO>
+{
+    public BlogGetDtoValidator()
+    {
+        RuleFor(x => x.Title).NotNull().NotEmpty().MaximumLength(200);
+        RuleFor(x => x.Description).NotNull().NotEmpty().MaximumLength(15000);
+    }
+}
---
+++ src/Core/EndProject.Application/Validators/BlogValidators/BlogUpdateDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Application.DTOs.Blog;
+using FluentValidation;
+
+namespace EndProject.Application.Validators.BlogValidators;
+
+public class BlogUpdateDtoValidator : AbstractValidator<BlogUpdateDTO>
+{
+    public BlogUpdateDtoValidator()
+    {
+        RuleFor(x => x.Title).NotNull().NotEmpty().MaximumLength(200);
+        RuleFor(x => x.Description).NotNull().NotEmpty().MaximumLength(15000);
+    }
+}
