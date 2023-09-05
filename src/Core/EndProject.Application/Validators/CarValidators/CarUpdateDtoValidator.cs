9ba081bdd8b235272b3e2efff8e964fa0242426c
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 03:47:44 +0400

    Car Validators

---
+++ src/Core/EndProject.Application/Validators/CarValidators/CarUpdateDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using EndProject.Application.DTOs.Car;
+using FluentValidation;
+
+namespace EndProject.Application.Validators.CarValidators;
+
+public class CarUpdateDtoValidator : AbstractValidator<CarUpdateDTO>
+{
+    public CarUpdateDtoValidator()
+    {
+        RuleFor(x => x.Marka).NotNull().NotEmpty().MaximumLength(130);
+        RuleFor(x => x.Model).NotNull().NotEmpty().MaximumLength(120);
+        RuleFor(x => x.Price).NotNull().NotEmpty();
+        RuleFor(x => x.Year).NotNull().NotEmpty();
+        RuleFor(x => x.Description).NotNull().NotEmpty().MaximumLength(1000);
+    }
+}
