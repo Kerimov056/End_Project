ffd24f467f265333c4b770ffc96c76ed92f74cb5
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 02:27:59 +0400

    CarCommentTypeConfigurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/CarCommentConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+internal class CarCommentConfigurations : IEntityTypeConfiguration<CarComment>
+{
+    public void Configure(EntityTypeBuilder<CarComment> builder)
+    {
+        builder.Property(x => x.Comment).IsRequired().HasMaxLength(1000);
+    }
+}
---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/ChauffeursConfigurations.cs
@@ -0,0 +1,16 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class ChauffeursConfigurations : IEntityTypeConfiguration<Chauffeurs>
+{
+    public void Configure(EntityTypeBuilder<Chauffeurs> builder)
+    {
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.Number).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.Price).IsRequired().HasMaxLength(5000);
+        builder.Property(x => x.imagePath).IsRequired().HasMaxLength(15000);
+    }
+}
