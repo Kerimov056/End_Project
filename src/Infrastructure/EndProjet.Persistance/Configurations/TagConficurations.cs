bd04bd33e6fbe8d81bf2c4a7399011a5a2f0af10
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 02:09:28 +0400

    TagConfigurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/TagConficurations.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class TagConficurations : IEntityTypeConfiguration<Tag>
+{
+    public void Configure(EntityTypeBuilder<Tag> builder)
+    {
+        builder.Property(x => x.tag).IsRequired().HasMaxLength(140);
+    }
+}
