bff7d82482f36849c9d89c2254a52871619b379f
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 02:08:38 +0400

    CarImageConfigurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/CarImageConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class CarImageConfigurations : IEntityTypeConfiguration<CarImage>
+{
+    public void Configure(EntityTypeBuilder<CarImage> builder)
+    {
+        builder.Property(x => x.imagePath).IsRequired().HasMaxLength(12200);
+    }
+}
