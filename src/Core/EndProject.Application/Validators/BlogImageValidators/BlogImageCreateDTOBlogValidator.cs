ad3247a6848e53af43616f93037fafe2ca88a0e9
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 03:08:26 +0400

    BlogImage Validators

---
+++ src/Core/EndProject.Application/Validators/BlogImageValidators/BlogImageCreateDTOBlogValidator.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Application.DTOs.Blog;
+using EndProject.Application.DTOs.BlogImage;
+using FluentValidation;
+
+namespace EndProject.Application.Validators.BlogImageValidators;
+
+public class BlogImageCreateDTOBlogValidator : AbstractValidator<BlogImageCreateDTO>
+{
+    public BlogImageCreateDTOBlogValidator()
+    {
+        RuleFor(x => x.imagePath).NotNull().NotEmpty();
+    }
+}
---
+++ src/Core/EndProject.Application/Validators/BlogImageValidators/BlogImageUpdateDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Application.DTOs.BlogImage;
+using FluentValidation;
+
+namespace EndProject.Application.Validators.BlogImageValidators;
+
+public class BlogImageUpdateDtoValidator : AbstractValidator<BlogImageUpdateDTO>
+{
+    public BlogImageUpdateDtoValidator()
+    {
+        RuleFor(x => x.imagePath).NotNull().NotEmpty();
+    }
+}
