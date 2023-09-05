cf4ebc49d47aabadb792d69a1e62ec426c49a8bd
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 02:06:58 +0400

    BlogImageConfigurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/BlogImageConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class BlogImageConfigurations : IEntityTypeConfiguration<BlogImage>
+{
+    public void Configure(EntityTypeBuilder<BlogImage> builder)
+    {
+        builder.Property(x => x.imagePath).IsRequired().HasMaxLength(12200);
+    }
+}
