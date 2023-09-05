c518b8e82e16ff9a81cdd1e7ab91a8201fa802aa
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 02:03:38 +0400

    AdvantageConfigurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/AdvantagesConficurations.cs
@@ -0,0 +1,14 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class AdvantagesConficurations : IEntityTypeConfiguration<Faq>
+{
+    public void Configure(EntityTypeBuilder<Faq> builder)
+    {
+        builder.Property(x => x.Title).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.Descrption).IsRequired().HasMaxLength(1000);
+    }
+}
