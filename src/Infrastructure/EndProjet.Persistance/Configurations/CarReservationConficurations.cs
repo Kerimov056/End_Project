9166040c91491b17f1b06d7e0d4b52a66505aa3b
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 02:38:53 +0400

    CarReservationConficurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/CarReservationConficurations.cs
@@ -0,0 +1,17 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class CarReservationConficurations : IEntityTypeConfiguration<CarReservation>
+{
+    public void Configure(EntityTypeBuilder<CarReservation> builder)
+    {
+        builder.Property(x => x.FullName).IsRequired().HasMaxLength(140);
+        builder.Property(x => x.Email).IsRequired().HasMaxLength(255);
+        builder.Property(x => x.Number).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.ImagePath).IsRequired().HasMaxLength(12000);
+        builder.Property(x => x.Notes).IsRequired().HasMaxLength(12000);
+    }
+}
