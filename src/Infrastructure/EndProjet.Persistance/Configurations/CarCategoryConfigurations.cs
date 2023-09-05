db11ed346b5c2c3b6084bc4e43e65466e069a1bb
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 02:30:28 +0400

    CarCategoryTypeConfigurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/CarCategoryConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class CarCategoryConfigurations : IEntityTypeConfiguration<CarCategory>
+{
+    public void Configure(EntityTypeBuilder<CarCategory> builder)
+    {
+        builder.Property(x => x.Category).IsRequired().HasMaxLength(100);
+    }
+}
