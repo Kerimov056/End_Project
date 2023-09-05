0584ab25339f78d75c37470369543d6c4682bf7a
Kerimov056 <ulvisk@code.edu.az>
2023-09-06 02:35:06 +0400

    CarConfigurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/CarConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class CarConfigurations : IEntityTypeConfiguration<Car>
+{
+    public void Configure(EntityTypeBuilder<Car> builder)
+    {
+        builder.Property(x => x.Marka).IsRequired().HasMaxLength(130);
+        builder.Property(x => x.Model).IsRequired().HasMaxLength(120);
+        builder.Property(x => x.Price).IsRequired().HasMaxLength(1000);
+        builder.Property(x => x.Year).IsRequired().HasMaxLength(1120);
+        builder.Property(x => x.Description).IsRequired().HasMaxLength(12000);
+    }
+}
