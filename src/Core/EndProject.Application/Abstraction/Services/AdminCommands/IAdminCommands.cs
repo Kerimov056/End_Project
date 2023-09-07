7606892e06e30f3dd0ed102919f090306d8f8602
Kerimov056 <ulvisk@code.edu.az>
2023-09-07 19:41:10 +0400

    IAdminCommands

---
+++ src/Core/EndProject.Application/Abstraction/Services/AdminCommands/IAdminCommands.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Domain.Entitys.Identity;
+
+namespace EndProject.Application.Abstraction.Services.AdminCommands;
+
+public interface IAdminCommands
+{
+    Task<List<AppUser>> AllMemberUser(string? searchUser);
+    Task<List<AppUser>> AllAdminUser(string? searchUser);
+    Task<AppUser> ByUser(string? userId);
+    Task AdminCreate(string superAdminId, string appUserId);
+    Task AdminDelete(string superAdminId, string appAdminId);
+    Task RemoveUser(string superAdminId, string userId);
+}
--- src/Core/EndProject.Application/Abstraction/Services/IAuthService.cs
+++ src/Core/EndProject.Application/Abstraction/Services/IAuthService.cs
@@ -1,20 +1,16 @@
-﻿using EndProject.Application.Abstraction.Services.SosicalAuthentications;
+﻿using EndProject.Application.Abstraction.Services.AdminCommands;
+using EndProject.Application.Abstraction.Services.SosicalAuthentications;
 using EndProject.Application.DTOs.Auth;
 using EndProject.Domain.Entitys.Identity;
 using EndProject.Domain.Helpers;
 
 namespace EndProject.Application.Abstraction.Services;
 
-public interface IAuthService : IExternalAuthentications
+public interface IAuthService : IExternalAuthentications, IAdminCommands
 {
     Task<SignUpResponse> Register(RegisterDTO registerDTO);
     Task<TokenResponseDTO> Login(LoginDTO loginDTO);
     Task<TokenResponseDTO> LoginAdmin(LoginDTO loginDTO);
     Task<TokenResponseDTO> ValidRefleshToken(string refreshToken);
-    Task<List<AppUser>> AllMemberUser(string? searchUser);
-    Task<List<AppUser>> AllAdminUser(string? searchUser);
-    Task<AppUser> ByUser(string? userId);
-    Task AdminCreate(string superAdminId, string appUserId);
-    Task AdminDelete(string superAdminId, string appAdminId);
-    Task RemoveUser(string superAdminId, string userId);
+
 }
