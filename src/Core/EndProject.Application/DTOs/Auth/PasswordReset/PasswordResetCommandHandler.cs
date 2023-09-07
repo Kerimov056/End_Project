ac2d6983f335120aefac166f65a503ea54a4f892
Kerimov056 <ulvisk@code.edu.az>
2023-09-07 20:12:36 +0400

    PasswordResetCommandHandler

---
+++ src/Core/EndProject.Application/DTOs/Auth/PasswordReset/PasswordResetCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using EndProject.Application.Abstraction.Services;
+using MediatR;
+
+namespace EndProject.Application.DTOs.Auth.PasswordReset;
+
+public class PasswordResetCommandHandler : IRequestHandler<PasswordResetCommandRequest, PasswordResetCommandResponse>
+{
+    readonly IAuthService _authService;
+
+    public PasswordResetCommandHandler(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    public async Task<PasswordResetCommandResponse> Handle(PasswordResetCommandRequest request, CancellationToken cancellationToken)
+    {
+        await _authService.PasswordResetAsnyc(request.Email);
+        return new();
+    }
+}
---
+++ src/Core/EndProject.Application/DTOs/Auth/PasswordReset/PasswordResetCommandResponse.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace EndProject.Application.DTOs.Auth.PasswordReset;
+
+public class PasswordResetCommandResponse
+{
+
+}
