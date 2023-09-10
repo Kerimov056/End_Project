fb3f4f81afc6a9083c47197a4abe4a91366e0240
Kerimov056 <ulvisk@code.edu.az>
2023-09-11 03:05:08 +0400

    Communication Repository

---
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Repositories/EntityRepository/CommunicationReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Domain.Entitys;
+using EndProjet.Persistance.Context;
+
+namespace EndProjet.Persistance.Implementations.Repositories.EntityRepository;
+
+public class CommunicationReadRepository : ReadRepository<Communication>, ICommunicationReadRepository
+{
+    public CommunicationReadRepository(AppDbContext context) : base(context)
+    {
+    }
+}
---
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Repositories/EntityRepository/CommunicationWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Application.Abstraction.Repositories.IEntityRepository;
+using EndProject.Domain.Entitys;
+using EndProjet.Persistance.Context;
+
+namespace EndProjet.Persistance.Implementations.Repositories.EntityRepository;
+
+public class CommunicationWriteRepository : WriteRepository<Communication>, ICommunicationWriteRepository
+{
+    public CommunicationWriteRepository(AppDbContext context) : base(context)
+    {
+    }
+}
