9466289c85ed467905042b83aa410af1ff6796f5
Kerimov056 <ulvisk@code.edu.az>
2023-09-11 03:35:44 +0400

    CommunicationsController

---
+++ src/Presentation/EndProject.API/Controllers/CommunicationsController.cs
@@ -0,0 +1,42 @@
+﻿using EndProject.Application.Abstraction.Services;
+using EndProject.Application.DTOs.Communication;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace EndProject.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommunicationsController : ControllerBase
+{
+    private readonly ICommunicationServices _communicationServices;
+    public CommunicationsController(ICommunicationServices communicationServices) => _communicationServices = communicationServices;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var allCommunications = await _communicationServices.GetAllAsync();
+        return Ok(allCommunications);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(CommunicationCreateDTO communicationCreateDTO)
+    {
+        await _communicationServices.CreateAsync(communicationCreateDTO);
+        return StatusCode((int)HttpStatusCode.Created);
+    }
+
+    [HttpGet("{id:Guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var BySlider = await _communicationServices.GetByIdAsync(id);
+        return Ok(BySlider);
+    }
+
+    [HttpDelete("{id:Guid}")]
+    public async Task<IActionResult> Remove(Guid id)
+    {
+        await _communicationServices.RemoveAsync(id);
+        return Ok();
+    }
+}
