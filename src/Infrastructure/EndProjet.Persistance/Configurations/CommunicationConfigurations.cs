ba7f4fe5153fd76d672c9d121faf47c7e025753a
Kerimov056 <ulvisk@code.edu.az>
2023-09-11 03:14:31 +0400

    CommunicationConfigurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/CommunicationConfigurations.cs
@@ -0,0 +1,17 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class CommunicationConfigurations : IEntityTypeConfiguration<Communication>
+{
+    public void Configure(EntityTypeBuilder<Communication> builder)
+    {
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.SurName).IsRequired().HasMaxLength(140);
+        builder.Property(x => x.Email).IsRequired().HasMaxLength(255);
+        builder.Property(x => x.Phone).IsRequired().HasMaxLength(30);
+        builder.Property(x => x.Note).IsRequired().HasMaxLength(3000);
+    }
+}
