5f6a3e24c9384b1d63a66f5c5b405666e096466d
Kerimov056 <ulvisk@code.edu.az>
2023-09-11 03:21:20 +0400

    CommunicationProfile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/CommunicationProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Communication;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class CommunicationProfile : Profile
+{
+    public CommunicationProfile()
+    {
+        CreateMap<Communication, CommunicationGetDTO>().ReverseMap();
+        CreateMap<Communication, CommunicationCreateDTO>().ReverseMap();
+    }
+}
