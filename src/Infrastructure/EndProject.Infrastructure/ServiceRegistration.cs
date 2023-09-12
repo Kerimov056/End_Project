85809ff87eadc9a59a3e37518f90953802e6125f
Kerimov056 <ulvisk@code.edu.az>
2023-09-13 01:12:49 +0400

    Payment Services  add ServiceRegistration

--- src/Infrastructure/EndProject.Infrastructure/ServiceRegistration.cs
+++ src/Infrastructure/EndProject.Infrastructure/ServiceRegistration.cs
@@ -1,10 +1,13 @@
 ﻿using EndProject.Application.Abstraction.Services;
+using EndProject.Application.Abstraction.Services.Payment;
 using EndProject.Application.Abstraction.Services.Stroge;
+using EndProject.Infrastructure.Services;
 using EndProject.Infrastructure.Services.Azure;
 using EndProject.Infrastructure.Services.Email;
 using EndProject.Infrastructure.Services.Stroge;
 using EndProject.Infrastructure.Services.Token;
 using Microsoft.Extensions.DependencyInjection;
+using Stripe;
 
 namespace EndProject.Infrastructure;
 
@@ -12,14 +15,27 @@ public static class ServiceRegistration
 {
     public static void AddInfrastructureServices(this IServiceCollection services)
     {
+        //File
+        //services.AddScoped<IStorgeService, StorgeService();
         services.AddScoped<ITokenHandler, TokenHandler>();
         services.AddScoped<IStorageFile, StorageFile>();
         services.AddScoped<AzureBlobService>();
         //services.AddScoped<ILocalStorage, LocalStorage>();
+
+        //Payment
+        services.AddScoped<IPaymentService, PaymentService>();
+        services.AddScoped<TokenService>();
+        services.AddScoped<CustomerService>();
+        services.AddScoped<ChargeService>();
+
     }
-                                                                                      //LocalStorage
+    //LocalStorage
     public static void AddStorageFile<T>(this IServiceCollection services) where T : Storage, IStorageFile
     {
         services.AddScoped<IStorageFile, T>(); 
+    }
+    public static void AddPayment<T>(this IServiceCollection services) where T : class, IPayment
+    {
+        services.AddScoped<IPayment, T>();
     }
 }
