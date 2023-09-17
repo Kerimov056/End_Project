0822269402fcf0a9773659a4dffaa3bf2177b509
Kerimov056 <ulvisk@code.edu.az>
2023-09-17 17:28:31 +0400

    WishlistsController

---
+++ src/Presentation/EndProject.API/Controllers/WishlistsController.cs
@@ -0,0 +1,51 @@
+﻿using EndProject.Application.Abstraction.Services;
+using EndProjet.Persistance.Implementations.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace EndProject.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WishlistsController : ControllerBase
+{
+    private readonly IWishlistServices _wishlistServices;
+
+    public WishlistsController(IWishlistServices wishlistServices)
+      =>  _wishlistServices = wishlistServices;
+
+    [HttpPost]
+    public async Task<IActionResult> AddBasket([Required][FromQuery] Guid Id, [Required][FromQuery] string AppUserId)
+    {
+        await _wishlistServices.AddWishlistAsync(Id, AppUserId);
+        return StatusCode((int)HttpStatusCode.Created);
+    }
+
+    //[HttpGet]
+    //public async Task<IActionResult> GetBasketProducts([Required][FromQuery] string AppUserId)
+    //{
+    //    return Ok(await _basketService.GetBasketProductsAsync(AppUserId));
+    //}
+
+    //[HttpDelete("Delete-Basket-Product")]
+    //public async Task<IActionResult> DeleteBasketProduct([Required][FromQuery] Guid Id, [Required][FromQuery] string AppUserId)
+    //{
+    //    await _basketService.DeleteBasketAsync(Id, AppUserId);          //Isdiyir aaa Roter'i duzelt
+    //    return Ok();
+    //}
+
+    //[HttpDelete("ProductItem")]
+    //public async Task<IActionResult> DeleteBasketItemProduct([Required][FromQuery] Guid Id, [Required][FromQuery] string AppUserId)
+    //{
+    //    await _basketService.DeleteBasketItemAsync(Id, AppUserId);
+    //    return Ok();
+    //}
+
+    //[HttpGet("Get-Basket-Count")]
+    //public async Task<IActionResult> GetBasketCount([Required][FromQuery] string AppUserId)  //baskete nece cur ferqli product oldugunu gosterir.
+    //{
+    //    var basketCount = await _basketService.GetBasketCountAsync(AppUserId);
+    //    return Ok(basketCount);
+    //}
+}
