795c0027fcc78183eb835298ab9c9174c47fe698
Kerimov056 <ulvisk@code.edu.az>
2023-09-17 16:47:22 +0400

    WishlistProfile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/WishlistProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Wishlist;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class WishlistProfile : Profile
+{
+    public WishlistProfile()
+    {
+        CreateMap<WishlistProduct, WishlistProductDto>().ReverseMap();
+    }
+}
