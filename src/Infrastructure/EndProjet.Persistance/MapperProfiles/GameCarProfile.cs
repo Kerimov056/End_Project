c927c2b3884a72b4bef9f3acc5d13a027d2f7999
Kerimov056 <ulvisk@code.edu.az>
2023-09-20 19:01:28 +0400

    GameCar Profile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/GameCarProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Game;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class GameCarProfile : Profile
+{
+    public GameCarProfile()
+    {
+        CreateMap<GameCar, GameCarCreateDTO>().ReverseMap();
+        CreateMap<GameCar, GameCarGetDTO>().ReverseMap();
+    }
+}
