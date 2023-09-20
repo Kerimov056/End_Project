652e9044bef26c8c23b016ed8c01dceaf6a6dc3d
Kerimov056 <ulvisk@code.edu.az>
2023-09-20 19:03:54 +0400

    GameCar Configurations

---
+++ src/Infrastructure/EndProjet.Persistance/Configurations/GameCarConfigurations.cs
@@ -0,0 +1,13 @@
+﻿using EndProject.Domain.Entitys;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EndProjet.Persistance.Configurations;
+
+public class GameCarConfigurations : IEntityTypeConfiguration<GameCar>
+{
+    public void Configure(EntityTypeBuilder<GameCar> builder)
+    {
+        builder.Property(x => x.Password).IsRequired().HasMaxLength(40);
+    }
+}
