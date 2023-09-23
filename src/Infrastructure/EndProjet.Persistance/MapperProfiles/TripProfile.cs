731f3de480e876aa246fbe4be9b5170480c49953
Kerimov056 <ulvisk@code.edu.az>
2023-09-24 00:29:28 +0400

    Trip Profile Mapper

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/TripProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Trip;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class TripProfile : Profile
+{
+    public TripProfile()
+    {
+        CreateMap<Trip, TripCreateDTO>().ReverseMap();
+        CreateMap<Trip, TripGetDTO>().ReverseMap();
+        CreateMap<Trip, TripUpdateDTO>().ReverseMap();
+    }
+}
