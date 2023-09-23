2efc1f98753b9c59693ba26d25f403a70ae06c8f
Kerimov056 <ulvisk@code.edu.az>
2023-09-24 00:58:30 +0400

    Trip Service CreateAsync && GetAllAsync

--- src/Core/EndProject.Application/Abstraction/Services/ITripServices.cs
+++ src/Core/EndProject.Application/Abstraction/Services/ITripServices.cs
@@ -4,7 +4,7 @@ namespace EndProject.Application.Abstraction.Services;
 
 public interface ITripServices
 {
-    Task<List<TripGetDTO>> GetAllAsync();
+    Task<List<TripGetDTO>> GetAllAsync(string AppUserId);
     Task CreateAsync(TripCreateDTO tripCreateDTO);
     Task<TripGetDTO> GetByIdAsync(Guid Id);
     Task UpdateAsync(Guid id, TripUpdateDTO tripUpdateDTO);
--- src/Infrastructure/EndProjet.Persistance/Implementations/Services/TripServices.cs
+++ src/Infrastructure/EndProjet.Persistance/Implementations/Services/TripServices.cs
@@ -1,7 +1,13 @@
 ﻿using AutoMapper;
 using EndProject.Application.Abstraction.Repositories.IEntityRepository;
 using EndProject.Application.Abstraction.Services;
+using EndProject.Application.DTOs.Game;
 using EndProject.Application.DTOs.Trip;
+using EndProject.Domain.Entitys;
+using EndProject.Domain.Entitys.Identity;
+using EndProjet.Persistance.Exceptions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace EndProjet.Persistance.Implementations.Services;
 
@@ -10,24 +16,41 @@ public class TripServices : ITripServices
     private readonly ITripeReadRepository _tripReadRepository;
     private readonly ITripeWriteRepository _tripWriteRepository;
     private readonly IMapper _mapper;
+    private readonly UserManager<AppUser> _userManager;
+
 
     public TripServices(ITripeReadRepository tripReadRepository,
                         ITripeWriteRepository tripWriteRepository,
-                        IMapper mapper)
+                        IMapper mapper,
+                        UserManager<AppUser> userManager)
     {
         _tripReadRepository = tripReadRepository;
         _tripWriteRepository = tripWriteRepository;
         _mapper = mapper;
+        _userManager = userManager;
     }
 
-    public Task CreateAsync(TripCreateDTO tripCreateDTO)
+    public async Task CreateAsync(TripCreateDTO tripCreateDTO)
     {
-        throw new NotImplementedException();
+        var appUser = await _userManager.FindByIdAsync(tripCreateDTO.AppUserId);
+        if (appUser is null) throw new NotFoundException("Not Found User");
+
+        var newTrip = _mapper.Map<Trip>(tripCreateDTO);
+        await _tripWriteRepository.AddAsync(newTrip);
+        await _tripWriteRepository.SavaChangeAsync();
     }
 
-    public Task<List<TripGetDTO>> GetAllAsync()
+    public async Task<List<TripGetDTO>> GetAllAsync(string AppUserId)
     {
-        throw new NotImplementedException();
+        var appUser = await _userManager.FindByIdAsync(AppUserId);
+        if (appUser is null) throw new NotFoundException("Not Found User");
+
+        var allTrips = await _tripReadRepository.GetAll()
+                       .Where(x => x.AppUserId == AppUserId)
+                       .ToListAsync();
+
+        var toDto = _mapper.Map<List<TripGetDTO>>(allTrips);    
+        return toDto;
     }
 
     public Task<TripGetDTO> GetByIdAsync(Guid Id)
