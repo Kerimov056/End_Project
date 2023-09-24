2a4bcdb1e9b56f882f16448b3f04c33769177e3e
Kerimov056 <ulvisk@code.edu.az>
2023-09-24 13:33:53 +0400

    TripNoteProfile

---
+++ src/Infrastructure/EndProjet.Persistance/MapperProfiles/TripNoteProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EndProject.Application.DTOs.Trip;
+using EndProject.Domain.Entitys;
+
+namespace EndProjet.Persistance.MapperProfiles;
+
+public class TripNoteProfile : Profile
+{
+    public TripNoteProfile()
+    {
+        CreateMap<TripNote, TripCreateDTO>().ReverseMap();
+        CreateMap<TripNote, TripGetDTO>().ReverseMap();
+        CreateMap<TripNote, TripUpdateDTO>().ReverseMap();
+    }
+}
